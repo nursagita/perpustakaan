c597dfee4064d09a705f64d97652a00a7ed49cab
nursagita <43537066+nursagita@users.noreply.github.com>
2018-11-17 20:44:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<ceter>
+		<table width="700" border="1">
+			<tr>
+				<td colspan="2" align="center"><h1>sistem informasi perpustakaan</h1>
+				</td>
+			</tr>
+			<td width="200">
+			<ul>
+			<li><a href="anggota.php">Angota</a></li>
+			<li><a href="buku.php">Buku</a></li>
+			<li><a href="pinjam.php">Pinjam</a></li>
+			<ul>
+
+			</td>
+			<td width = "200">selamat datang di sistem perpustakaan</td>
+			</tr>
+			<tr>
+			<td colspan="2" align="center">Nursagita</td>
+			</tr>
+			</table>
+			</center> 
+
+</body>
+</html>
